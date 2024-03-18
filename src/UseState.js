c1a2d330f2f386ea233ba2657c8901bca208654f
Sebastian Cabal <sebascabalq@gmail.com>
2024-03-18 08:06:10 -0500

    Implementacion de estados

--- src/UseState.js
+++ src/UseState.js
@@ -1,12 +1,33 @@
 import React from 'react'
 
-function UseState() {
+function UseState({ name }) {
+  const [error, setError] = React.useState(true);
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    console.log("Empezando el efecto");
+    if (!!loading) {
+      setTimeout(() => {
+        console.log("Haciendo la validación");
+        setLoading(false);
+        console.log("Terminando la validación");
+      }, 3000);
+    }
+    console.log("Terminando el efecto");
+
+  }, [loading])
   return (
     <div>
-      <h2>Eliminar UseState</h2>
+      <h2>Eliminar {name}</h2>
       <p>Por favor, escribe el código de seguridad.</p>
+      {error && (
+        <p>Error: el código es incorrecto</p>
+      )}
+      {loading && (
+        <p>Cargando ...</p>
+      )}
       <input placeholder='Código de seguridad' />
-      <button>Comprobar</button>
+      <button onClick={() => setLoading(true)}>Comprobar</button>
     </div>
   )
 }
